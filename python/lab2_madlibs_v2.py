fd6ce4c077db286fee9e552baa99ead4b86dd5ab
Harry Shin <harrry@Harrys-MacBook-Pro.local>
2020-12-15 21:08:10 -0800

    added lab 2 version 2

---
+++ python/lab2_madlibs_v2.py
@@ -0,0 +1,28 @@
+"""
+Creator: Harry Shin
+Date: 15 Dec 2020
+"""
+import random
+
+noun_list = input("Please enter three nouns separated by commas: ")
+adj_list = input("Please enter three adjectives separated by commas: ")
+verb_list = input("Please enter two verbs separated by commas: ")
+
+snl = noun_list.split(', ')
+sal = adj_list.split(', ')
+svl = verb_list.split(', ')
+
+# snl = random.choice(snl)
+# sal = random.choice(sal)
+# svl = random.choice(svl)
+
+print(f'''
+No animal must ever live in a {random.choice(snl)}, or sleep in
+a {random.choice(snl)}, or wear clothes, or drink {random.choice(snl)}, or
+smoke tobacco, or touch money, or engage in
+trade. The habits of Man are {random.choice(sal)}. And, above
+all, no animal must ever {random.choice(svl)} over his own
+kind. Weak or {random.choice(sal)}, clever or {random.choice(sal)}, we are all
+brothers. No animal must ever {random.choice(svl)} any other
+animal. All animals are equal.
+''')
