30e119ac9f16d6bae5b33da6c5c2deb2b844567e
Harry Shin <harrry@Harrys-MacBook-Pro.local>
2020-12-15 20:26:07 -0800

    added lab2_madlibs.py

---
+++ python/lab2_madlibs.py
@@ -0,0 +1,24 @@
+"""
+Creator: Harry Shin
+Date: 15 Dec 2020
+"""
+
+noun1 = input("Please enter your first noun: ")
+noun2 = input("Please enter your second noun: ")
+noun3 = input("Please enter your third noun: ")
+adj1 = input("Please enter your first adjective: ")
+verb1 = input("Please enter your first verb: ")
+adj2 = input("Please enter your second adjective: ")
+adj3 = input("Please enter your third adjective: ")
+verb2 = input("Please enter your second verb: ")
+
+print(f'''
+No animal must ever live in a {noun1}, or sleep in
+a {noun2}, or wear clothes, or drink {noun3}, or
+smoke tobacco, or touch money, or engage in
+trade. The habits of Man are {adj1}. And, above
+all, no animal must ever {verb1} over his own
+kind. Weak or {adj2}, clever or {adj3}, we are all
+brothers. No animal must ever {verb2} any other
+animal. All animals are equal.
+''')
